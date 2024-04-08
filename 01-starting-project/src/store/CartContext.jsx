2411a4df92a433f1c5ece8ced1b79c85d0abf9d1
hvrishabh <hitesh.verma@rishabhsoft.com>
2024-04-08 17:28:03 +0530

    the first ecomm commit

---
+++ 01-starting-project/src/store/CartContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    if (existingCartItemIndex > -1) {
+    }
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //..update the state to remove a meal item
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+}
+
+export default CartContext;
